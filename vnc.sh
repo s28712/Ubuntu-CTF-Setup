d26e3556af95364c739d4fbfddedf07f1a8c8d67
simar-singh <55468002+simar-singh@users.noreply.github.com>
2021-05-18 18:35:24 -0400

    Create vnc.sh

---
+++ vnc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer -y
+mkdir -p /home/ubuntu/.vnc
+touch /home/ubuntu/.vnc/passwd
+echo passwd | vncpasswd -f > ~/.vnc/passwd
+chown -R ubuntu:ubuntu /home/ubuntu/.vnc
+chmod 0600 /home/ubuntu/.vnc/passwd
+touch /home/ubuntu/.vnc/xstartup
