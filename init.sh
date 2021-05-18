ce01a1050e58b995c2a7813cd8c96bea0ee6f6d3
simar-singh <55468002+simar-singh@users.noreply.github.com>
2021-05-18 18:56:01 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
-multipass launch -n reptilia -c 2 -d 20G -m 4G
-multipass exec reptilia -- sudo apt update -y
-multipass exec reptilia -- sudo apt install net-tools dialog apt-utils -y
-echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-sudo apt-get install -y
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+apt-get install -y
