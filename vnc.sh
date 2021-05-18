d91b016029a61cb5c247667f95191faf2f7a3fa5
simar-singh <55468002+simar-singh@users.noreply.github.com>
2021-05-18 18:43:14 -0400

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer -y
+sudo apt install tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension tigervnc-viewer ubuntu-desktop -y
 mkdir -p /home/ubuntu/.vnc
 touch /home/ubuntu/.vnc/passwd
 echo passwd | vncpasswd -f > ~/.vnc/passwd
