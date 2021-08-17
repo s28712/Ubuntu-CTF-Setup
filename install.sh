51ac3b61da6d18dbc267a568de6fcc54902c19a1
simar-singh <55468002+simar-singh@users.noreply.github.com>
2021-08-16 23:10:25 -0400

    Removed Obsolete packages
    Removed packages that no longer are used or exist in version

--- install.sh
+++ install.sh
@@ -47,31 +47,8 @@ apt install -y netcat \
 git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
 ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
 
-# Install msfconsole
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-  chmod 755 msfinstall && \
-  ./msfinstall
-rm msfinstall
-
-# Install wpscan
-gem install wpscan
 
 # Clone git repos
 git clone https://github.com/danielmiessler/SecLists.git /opt/seclists
-git clone https://github.com/PowerShellMafia/PowerSploit.git /opt/powersploit
-git clone https://github.com/EmpireProject/Empire.git /opt/Empire
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/peas
 git clone https://github.com/pentestmonkey/php-reverse-shell.git /opt/php-reverse-shell
-
-# pip3 installs
-pip3 install pwntools
-
-# install gef and tools
-curl -s -L https://github.com/hugsy/gef/raw/master/scripts/gef.sh | sh
-pip3 install capstone
-pip3 install ropper
-pip3 install angr
-
-# use snap to install rest of the tools
-snap install spek --classic
-snap install gimp --classic
